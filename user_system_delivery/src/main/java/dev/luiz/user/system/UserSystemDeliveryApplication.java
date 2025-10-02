78c0f31786283d05f90f143df4a5f79589655d34
Luiz Sales <soutoluiz0609@gmail.com>
2025-10-02 15:58:00 -0300

    User system first commit

---
+++ user_system_delivery/src/main/java/dev/luiz/user/system/UserSystemDeliveryApplication.java
@@ -0,0 +1,13 @@
+package dev.luiz.user.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSystemDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSystemDeliveryApplication.class, args);
+	}
+
+}
