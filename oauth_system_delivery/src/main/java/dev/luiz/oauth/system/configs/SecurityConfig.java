a2bd6728e14cc38056a3914ac07b05b10d64fb17
Luiz Sales <soutoluiz0609@gmail.com>
2025-12-09 17:50:52 -0300

    code adjustment

--- oauth_system_delivery/src/main/java/dev/luiz/oauth/system/configs/SecurityConfig.java
+++ oauth_system_delivery/src/main/java/dev/luiz/oauth/system/configs/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import dev.luiz.oauth.system.services.UserDetailService;
@@ -20,12 +21,14 @@ public class SecurityConfig {
 	
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    	
+
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                                .anyRequest().permitAll() 
-                );
+                .sessionManagement(management -> management
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeRequests(requests -> requests
+                        .anyRequest().permitAll());
+
         return http.build();
     }
     
